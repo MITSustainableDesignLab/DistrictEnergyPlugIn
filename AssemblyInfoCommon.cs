80ea23bea0697f9ebfe389e83d9ad7ea9a95c35f
Samuel Letellier-Duchesne <samuel.letellier-duchesne@polymtl.ca>
2017-04-07 15:14:53 -0400

    Moved stuff arround

---
+++ AssemblyInfoCommon.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Polytechnique Montreal")]
+[assembly: AssemblyCopyright("Copyright © Polytechnique Montreal 2017")]
+[assembly: AssemblyProduct("DistricEnergyPlugIn")]
+
+[assembly: AssemblyVersion("1.0.0")]
