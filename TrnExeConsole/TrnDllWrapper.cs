71d94d8f50cbe5b68699eb0cb4401378726114d2
Samuel Letellier-Duchesne <samuel.letellier-duchesne@polymtl.ca>
2017-06-22 00:13:03 -0400

    Some more refactoring

--- TrnExeConsole/TrnDllWrapper.cs
+++ TrnExeConsole/TrnDllWrapper.cs
@@ -1,14 +1,27 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace TrnsysConsoleApp
+namespace TrnExeConsole
 {
+    public class GetTrnsysCallOutputs
+    {
+        public GetTrnsysCallOutputs(double[] parOut, double[] plotOut, int callType)
+        {
+            ParOut = parOut;
+            PlotOut = plotOut;
+            CallType = callType;
+        }
+
+        public double[] ParOut { get; }
+        public double[] PlotOut { get; }
+        public int CallType { get; }
+    }
+
     public class TrnDllWrapper
     {
         // Delegate with function signature for the Trnsys function
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-        //[return: MarshalAs(UnmanagedType.FunctionPtr)]
-        delegate void TrnsysDelegate(
+        private delegate void TrnsysDelegate(
             [Out] [In] ref int callType,
             [Out] [In] double[] parout,
             [Out] [In] double[] plotout,
@@ -16,27 +29,27 @@ namespace TrnsysConsoleApp
             [Out] [In] char[] titles,
             [Out] [In] char[] deckn);
 
-        public int Trnsys(int callType, out double[] parOut, out double[] plotOut, string deckn)
+        public GetTrnsysCallOutputs Trnsys(int callType, string deckn)
         {
+            double[] parOut;
+            double[] plotOut;
 
             if (_trnsys != null)
             {
                 //Arguments used when calling TRNSYS in TRNDll
                 var lpa = 1000;
                 var lpl = 1000;
-                double[] parout = new double[lpa];
-                double[] plotout = new double[lpl];
+                var parout = new double[lpa];
+                var plotout = new double[lpl];
                 var lengthLabels = 4000;
                 var lengthTitles = 1500;
                 var lengthDeckn = 300;
-                char[] labels = new char[lengthLabels];
-                char[] titles = new char[lengthTitles];
-                char[] deckPath = new char[lengthDeckn];
+                var labels = new char[lengthLabels];
+                var titles = new char[lengthTitles];
+                var deckPath = new char[lengthDeckn];
 
-                for (int i = 0; i < deckn.Length; i++)
-                {
+                for (var i = 0; i < deckn.Length; i++)
                     deckPath[i] = deckn[i];
-                }
 
                 //Trnsys call
                 _trnsys(ref callType, parout, plotout, labels, titles, deckPath);
@@ -45,35 +58,29 @@ namespace TrnsysConsoleApp
                 plotOut = plotout;
 
                 // Return string
-                return callType;
+                return new GetTrnsysCallOutputs(parOut, plotOut, callType);
             }
-            parOut = null;
-            plotOut = null;
-            return -2;
+            return new GetTrnsysCallOutputs(null, null, -2);
         }
 
-        public TrnDllWrapper(string filename)
+        public TrnDllWrapper()
         {
-            var trnDll = NativeLibrary.GetLibraryPathname(@"C:\Trnsys\17-2-Bee\Compilers\Ivf15 (Vs2013)\TRNDll\Debug\TRNDll.dll");
+            var trnDll =
+                NativeLibrary.GetLibraryPathname(
+                    @"C:\Trnsys\17-2-Bee\Compilers\Ivf15 (Vs2013)\TRNDll\Debug\TRNDll.dll");
             _dllhandle = NativeLibrary.LoadLibrary(trnDll);
 
             if (_dllhandle == IntPtr.Zero)
-            {
                 return;
-            }
 
-            var trnsysHandle = NativeLibrary.GetProcAddress(_dllhandle, "TRNSYS");
+            IntPtr trnsysHandle = NativeLibrary.GetProcAddress(_dllhandle, "TRNSYS");
 
             if (trnsysHandle != IntPtr.Zero)
-            {
-                _trnsys = (TrnsysDelegate)Marshal.GetDelegateForFunctionPointer(
+                _trnsys = (TrnsysDelegate) Marshal.GetDelegateForFunctionPointer(
                     trnsysHandle,
                     typeof(TrnsysDelegate));
-            }
             else
-            {
                 Console.WriteLine("Fatal error. Routine named Trnsys not found in TRNDll");
-            }
         }
 
         ~TrnDllWrapper()
@@ -84,16 +91,10 @@ namespace TrnsysConsoleApp
             NativeLibrary.FreeLibrary(_dllhandle);
         }
 
-        // Handles and delegates
+        // Handles and delegates values
 
 
-        IntPtr _dllhandle = IntPtr.Zero;
-        TrnsysDelegate _trnsys = null;
-
-        public class GetTrnsysCallOutputs
-        {
-            public double[] parOut { get; set; }
-            public double[] plotOut { get; set; }
-        }
+        private readonly IntPtr _dllhandle;
+        private readonly TrnsysDelegate _trnsys;
     }
 }
