09e29612c79e88d8c65e57493561c19fbf92f10a
Unknown <samuelduchesne@me.com>
2018-02-27 11:08:26 -0500

    Removing deprecaded files

--- DHEngine/docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:50,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to District Energy PlugIn&#8217;s documentation!"],titleterms:{district:0,document:0,energi:0,indic:0,plugin:0,tabl:0,welcom:0}})
