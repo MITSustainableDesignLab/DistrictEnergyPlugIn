66c45a46850431ecc25529461647c2b6be09a6c1
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2018-03-12 17:06:27 -0400

    Validation box class

--- DistrictEnergy/Networks/ThermalPlants/WindTurbine.cs
+++ DistrictEnergy/Networks/ThermalPlants/WindTurbine.cs
@@ -1,5 +1,8 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
 using System.Runtime.Serialization;
+using System.Windows.Controls;
 
 namespace DistrictEnergy.Networks.ThermalPlants
 {
@@ -40,5 +43,39 @@ namespace DistrictEnergy.Networks.ThermalPlants
         /// </summary>
         [DataMember]
         [DefaultValue(0.15)] public double LOSS_WND { get; set; } = 0.15;
+    }
+
+    public class DoubleRangeRule : ValidationRule
+    {
+        public double Min { get; set; }
+
+        public double Max { get; set; }
+
+        public override ValidationResult Validate(object value,
+            CultureInfo cultureInfo)
+        {
+            double parameter = 0;
+
+            try
+            {
+                if (((string)value).Length > 0)
+                {
+                    parameter = Double.Parse((String)value);
+                }
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, "Illegal characters or "
+                                                   + e.Message);
+            }
+
+            if ((parameter < this.Min) || (parameter > this.Max))
+            {
+                return new ValidationResult(false,
+                    "Please enter value in the range: "
+                    + this.Min + " - " + this.Max + ".");
+            }
+            return new ValidationResult(true, null);
+        }
     }
 }
