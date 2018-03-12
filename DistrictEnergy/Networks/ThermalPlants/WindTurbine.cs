8a947a4e84de9c94d88fb9cac5334bd6945d8044
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2018-03-12 19:31:09 -0400

    Removed unecessary class

--- DistrictEnergy/Networks/ThermalPlants/WindTurbine.cs
+++ DistrictEnergy/Networks/ThermalPlants/WindTurbine.cs
@@ -44,38 +44,4 @@ namespace DistrictEnergy.Networks.ThermalPlants
         [DataMember]
         [DefaultValue(0.15)] public double LOSS_WND { get; set; } = 0.15;
     }
-
-    public class DoubleRangeRule : ValidationRule
-    {
-        public double Min { get; set; }
-
-        public double Max { get; set; }
-
-        public override ValidationResult Validate(object value,
-            CultureInfo cultureInfo)
-        {
-            double parameter = 0;
-
-            try
-            {
-                if (((string)value).Length > 0)
-                {
-                    parameter = Double.Parse((String)value);
-                }
-            }
-            catch (Exception e)
-            {
-                return new ValidationResult(false, "Illegal characters or "
-                                                   + e.Message);
-            }
-
-            if ((parameter < this.Min) || (parameter > this.Max))
-            {
-                return new ValidationResult(false,
-                    "Please enter value in the range: "
-                    + this.Min + " - " + this.Max + ".");
-            }
-            return new ValidationResult(true, null);
-        }
-    }
 }
