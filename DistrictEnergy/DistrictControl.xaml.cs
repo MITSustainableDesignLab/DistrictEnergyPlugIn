6bebd4bbcd61295500f2bc341c18685ca3d344a1
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2020-03-25 18:49:26 -0400

    This

--- DistrictEnergy/DistrictControl.xaml.cs
+++ DistrictEnergy/DistrictControl.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
+using LiveCharts;
 using LiveCharts.Wpf;
 using Rhino;
 using DistrictEnergy.ViewModels;
