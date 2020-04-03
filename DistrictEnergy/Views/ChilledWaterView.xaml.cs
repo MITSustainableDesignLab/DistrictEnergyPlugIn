14e074213a1ed2559ef52acc9b421942fa83b493
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2020-04-03 12:29:54 -0400

    Adds separate user-controls for plantsettings

---
+++ DistrictEnergy/Views/ChilledWaterView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DistrictEnergy.Views
+{
+    /// <summary>
+    ///     Interaction logic for AbsorptionChillerView.xaml
+    /// </summary>
+    public partial class AbsorptionChillerView : UserControl
+    {
+        public AbsorptionChillerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DistrictEnergy/Views/ElectricGenerationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DistrictEnergy.Views
+{
+    /// <summary>
+    ///     Interaction logic for ElectricGenerationView.xaml
+    /// </summary>
+    public partial class ElectricGenerationView : UserControl
+    {
+        public ElectricGenerationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
