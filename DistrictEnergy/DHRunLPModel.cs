df67c450afe1f82f2581b999dfca2ef5f12948dd
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2020-04-09 16:07:29 -0400

    Adds a simple Linear Program

---
+++ DistrictEnergy/DHRunLPModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Google.OrTools.LinearSolver;
+using Rhino;
+using Rhino.Commands;
+
+namespace DistrictEnergy
+{
+    public class DHRunLPModel : Command
+    {
+        static DHRunLPModel _instance;
+        public DHRunLPModel()
+        {
+            _instance = this;
+        }
+
+        ///<summary>The only instance of the DHRunLPModel command.</summary>
+        public static DHRunLPModel Instance => _instance;
+
+        public override string EnglishName => "DHRunLPModel";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            Main();
+            return Result.Success;
+        }
+
+        static void Main()
+        {
+            // Create the linear solver with the GLOP backend.
+            Solver solver = Solver.CreateSolver("SimpleLpProgram", "GLOP_LINEAR_PROGRAMMING");
+
+            // Create the variables x and y.
+            Variable x = solver.MakeNumVar(0.0, 1.0, "x");
+            Variable y = solver.MakeNumVar(0.0, 2.0, "y");
+
+            RhinoApp.WriteLine("Number of variables = " + solver.NumVariables());
+
+            // Create a linear constraint, 0 <= x + y <= 2.
+            Constraint ct = solver.MakeConstraint(0.0, 2.0, "ct");
+            ct.SetCoefficient(x, 1);
+            ct.SetCoefficient(y, 1);
+
+            RhinoApp.WriteLine("Number of constraints = " + solver.NumConstraints());
+
+            // Create the objective function, 3 * x + y.
+            Objective objective = solver.Objective();
+            objective.SetCoefficient(x, 3);
+            objective.SetCoefficient(y, 1);
+            objective.SetMaximization();
+
+            solver.Solve();
+
+            RhinoApp.WriteLine("Solution:");
+            RhinoApp.WriteLine("Objective value = " + solver.Objective().Value());
+            RhinoApp.WriteLine("x = " + x.SolutionValue());
+            RhinoApp.WriteLine("y = " + y.SolutionValue());
+        }
+    }
+}
