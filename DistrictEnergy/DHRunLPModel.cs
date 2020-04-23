f7dad45ac708e4eceae13f675bdef6d2755a1230
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2020-04-23 14:08:04 -0400

    Simple MIP Program

--- DistrictEnergy/DHRunLPModel.cs
+++ DistrictEnergy/DHRunLPModel.cs
@@ -25,74 +25,45 @@ namespace DistrictEnergy
             return Result.Success;
         }
 
-        class DataModel
-        {
-            public double[,] ConstraintCoeffs =
-            {
-                {5, 7, 9, 2, 1},
-                {18, 4, -9, 10, 12},
-                {4, 7, 3, 8, 5},
-                {5, 13, 16, 3, -7},
-            };
-
-            public double[] Bounds = {250, 285, 211, 315};
-            public double[] ObjCoeffs = {7, 8, 2, 9, 6};
-            public int NumVars = 5;
-            public int NumConstraints = 4;
-        }
-
         static void Main()
         {
-            DataModel data = new DataModel();
             // Create the linear solver with the CBC backend.
             Solver solver = Solver.CreateSolver("SimpleMipProgram", "CBC_MIXED_INTEGER_PROGRAMMING");
-            Variable[] x = new Variable[data.NumVars];
-            for (int j = 0; j < data.NumVars; j++)
-            {
-                x[j] = solver.MakeIntVar(0.0, double.PositiveInfinity, String.Format("x_{0}", j));
-            }
+
+            // x and y are integer non-negative variables.
+            Variable x = solver.MakeIntVar(0.0, double.PositiveInfinity, "x");
+            Variable y = solver.MakeIntVar(0.0, double.PositiveInfinity, "y");
 
             RhinoApp.WriteLine("Number of variables = " + solver.NumVariables());
 
-            for (int i = 0; i < data.NumConstraints; ++i)
-            {
-                Constraint constraint = solver.MakeConstraint(0, data.Bounds[i], "");
-                for (int j = 0; j < data.NumVars; ++j)
-                {
-                    constraint.SetCoefficient(x[j], data.ConstraintCoeffs[i, j]);
-                }
-            }
+            // x + 7 * y <= 17.5.
+            solver.Add(x + 7 * y <= 17.5);
+
+            // x <= 3.5.
+            solver.Add(x <= 3.5);
 
             RhinoApp.WriteLine("Number of constraints = " + solver.NumConstraints());
 
-            var objective = solver.Objective();
-            for (int j = 0; j < data.NumVars; ++j)
+            // Maximize x + 10 * y.
+            solver.Maximize(x + 10 * y);
+
+            Solver.ResultStatus resultStatus = solver.Solve();
+
+            // Check that the problem has an optimal solution.
+            if (resultStatus != Solver.ResultStatus.OPTIMAL)
             {
-                objective.SetCoefficient(x[j], data.ObjCoeffs[j]);
-                objective.SetMaximization();
-
-                Solver.ResultStatus resultStatus = solver.Solve();
-
-                // Check that the problem has an optimal solution.
-                if (resultStatus != Solver.ResultStatus.OPTIMAL)
-                {
-                    RhinoApp.WriteLine("The problem does not have an optimal solution!");
-                    return;
-                }
-
-                RhinoApp.WriteLine("Solution:");
-                RhinoApp.WriteLine("Optimal objective value = " + solver.Objective().Value());
-
-                for (int k = 0; k < data.NumVars; ++k)
-                {
-                    RhinoApp.WriteLine("x[" + k + "] = " + x[k].SolutionValue());
-                }
-
-                RhinoApp.WriteLine("\nAdvanced usage:");
-                RhinoApp.WriteLine("Problem solved in " + solver.WallTime() + " milliseconds");
-                RhinoApp.WriteLine("Problem solved in " + solver.Iterations() + " iterations");
-                RhinoApp.WriteLine("Problem solved in " + solver.Nodes() + " branch-and-bound nodes");
+                RhinoApp.WriteLine("The problem does not have an optimal solution!");
+                return;
             }
+            RhinoApp.WriteLine("Solution:");
+            RhinoApp.WriteLine("Objective value = " + solver.Objective().Value());
+            RhinoApp.WriteLine("x = " + x.SolutionValue());
+            RhinoApp.WriteLine("y = " + y.SolutionValue());
+
+            RhinoApp.WriteLine("\nAdvanced usage:");
+            RhinoApp.WriteLine("Problem solved in " + solver.WallTime() + " milliseconds");
+            RhinoApp.WriteLine("Problem solved in " + solver.Iterations() + " iterations");
+            RhinoApp.WriteLine("Problem solved in " + solver.Nodes() + " branch-and-bound nodes");
         }
     }
 }
