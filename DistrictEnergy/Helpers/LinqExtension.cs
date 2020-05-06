18385cdaed34439843c38f418317a5e054808031
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2020-05-06 17:09:17 -0400

    Reorders LoadsView (smaller more stable loads near center)

--- DistrictEnergy/Helpers/LinqExtension.cs
+++ DistrictEnergy/Helpers/LinqExtension.cs
@@ -21,6 +21,7 @@ namespace DistrictEnergy.Helpers
                 startDate = startDate.AddHours(period);
                 i += 1;
             }
+
             return seriesBuilder;
         }
 
@@ -30,7 +31,7 @@ namespace DistrictEnergy.Helpers
             var startDate = new DateTime(2018, 01, 01, 0, 0, 0);
             // Create SeriesBuilder
             var seriesBuilder = new List<DateTimePoint>();
-            var len= d.Count();
+            var len = d.Count();
             var hours = 8760 / len;
             // Iterate over each element of the array of results & create datetime index incrementally
             foreach (var x1 in d)
@@ -38,6 +39,7 @@ namespace DistrictEnergy.Helpers
                 seriesBuilder.Add(new DateTimePoint(startDate, x1));
                 startDate = startDate.AddHours(hours);
             }
+
             return seriesBuilder;
         }
 
@@ -51,9 +53,20 @@ namespace DistrictEnergy.Helpers
             return source.Select(o => o.Value).Max();
         }
 
+        public static double Min(this IEnumerable<DateTimePoint> source)
+        {
+            return source.Select(o => o.Value).Min();
+        }
+
         public static double Average(this IEnumerable<DateTimePoint> source)
         {
             return source.Select(o => o.Value).Average();
+        }
+
+        public static double Variance(this IEnumerable<DateTimePoint> values)
+        {
+            return values.Select(x => x.Value).ToList()
+                .Variance(values.Select(x => x.Value).ToList().Mean(), 0, values.Count());
         }
 
         public static IEnumerable<IEnumerable<T>> Batch<T>(
@@ -83,10 +96,65 @@ namespace DistrictEnergy.Helpers
         {
             var count = source.Count();
             var thisMany = count / n;
-            for (var i = 0; i < (float)n; i++)
+            for (var i = 0; i < (float) n; i++)
             {
                 yield return source.Skip(i * thisMany).Take(thisMany);
             }
+        }
+
+        public static double Mean(this List<double> values)
+        {
+            return values.Count == 0 ? 0 : values.Mean(0, values.Count);
+        }
+
+        public static double Mean(this List<double> values, int start, int end)
+        {
+            double s = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                s += values[i];
+            }
+
+            return s / (end - start);
+        }
+
+        public static double Variance(this List<double> values)
+        {
+            return values.Variance(values.Mean(), 0, values.Count);
+        }
+
+        public static double Variance(this List<double> values, double mean)
+        {
+            return values.Variance(mean, 0, values.Count);
+        }
+
+        public static double Variance(this List<double> values, double mean, int start, int end)
+        {
+            double variance = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                variance += Math.Pow((values[i] - mean), 2);
+            }
+
+            int n = end - start;
+            if (start > 0) n -= 1;
+
+            return variance / (n);
+        }
+
+        public static double StandardDeviation(this List<double> values)
+        {
+            return values.Count == 0 ? 0 : values.StandardDeviation(0, values.Count);
+        }
+
+        public static double StandardDeviation(this List<double> values, int start, int end)
+        {
+            double mean = values.Mean(start, end);
+            double variance = values.Variance(mean, start, end);
+
+            return Math.Sqrt(variance);
         }
     }
 }
--- DistrictEnergy/Networks/ThermalPlants/Dispatchable.cs
+++ DistrictEnergy/Networks/ThermalPlants/Dispatchable.cs
@@ -9,25 +9,8 @@ using Newtonsoft.Json;
 
 namespace DistrictEnergy.Networks.ThermalPlants
 {
-    public abstract class Dispatchable : IThermalPlantSettings
+    public abstract class Dispatchable : NotStorage
     {
-        public abstract LoadTypes InputType { get; }
-        public abstract double F { get; set; }
-        public abstract double V { get; set; }
-        public abstract double Capacity { get; set; }
-        public abstract string Name { get; set; }
-        public abstract Guid Id { get; set; }
-        public abstract LoadTypes OutputType { get; }
-        public abstract Dictionary<LoadTypes, double> ConversionMatrix { get; }
-        public abstract List<DateTimePoint> Input { get; set; }
-        public abstract List<DateTimePoint> Output { get; set; }
-        public abstract double Efficiency { get; }
-        public abstract Dictionary<LoadTypes, SolidColorBrush> Fill { get; set; }
-        public GraphCost FixedCost => new FixedCost(this);
-        public GraphCost VariableCost => new VariableCost(this, 200);
-        public double TotalCost => FixedCost.Cost + VariableCost.Cost;
-        public abstract double CapacityFactor { get; set; }
-        public abstract bool IsForced { get; set; }
     }
 
     public class FixedCost : GraphCost
