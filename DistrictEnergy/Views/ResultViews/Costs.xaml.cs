fb8b6587deff59cb346d5f6ffe1e5a69cda19823
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-03-29 13:48:13 -0400

    Fixes issue with Costs UserControl

--- DistrictEnergy/Views/ResultViews/Costs.xaml.cs
+++ DistrictEnergy/Views/ResultViews/Costs.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using DistrictEnergy.Annotations;
 using DistrictEnergy.ViewModels;
 
 namespace DistrictEnergy.Views.ResultViews
@@ -6,12 +9,20 @@ namespace DistrictEnergy.Views.ResultViews
     /// <summary>
     /// Interaction logic for Costs.xaml
     /// </summary>
-    public partial class Costs : UserControl
+    public partial class Costs : UserControl, INotifyPropertyChanged
     {
         public Costs()
         {
             InitializeComponent();
             DataContext = new CostsViewModel();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
